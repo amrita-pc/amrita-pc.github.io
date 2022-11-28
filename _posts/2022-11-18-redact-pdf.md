b93d67d84a444732772c37fc56525ae09894ca0e
Amrita <amrita.mitra93@gmail.com>
2022-11-29 00:30:43 +1100

    adding files to github pages

---
+++ _posts/2022-11-18-redact-pdf.md
@@ -0,0 +1,21 @@
+---
+layout: single
+title:  "Redacting sensitive information from PDF"
+date:   2022-11-18
+tags:
+     - pdf
+classes: wide
+# categories:
+#   - blogging
+header:
+#   image: assets/images/github-pages-jekyll.jpg
+#  teaser: assets/images/github-pages-jekyll.jpg
+#  overlay_filter: "0.5"
+#  overlay_image: /assets/splash/coffee.jpeg
+#  actions:
+#    - label: "Download"
+#      url: "#test-link"
+---
+
+{% gist b4bc7c0df300f56d78a130745b5810ea %}
+
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Redacting sensitive information from PDF",
+        "excerpt":"  ","categories": [],
+        "tags": ["pdf"],
+        "url": "/redact-pdf/",
+        "teaser": null
+      },{
+        "title": "dotfiles",
+        "excerpt":"  ","categories": [],
+        "tags": [".bashrc"],
+        "url": "/posts/.bashrc",
+        "teaser": null
+      },{
+        "title": "awk",
+        "excerpt":"Posts are stored in the _posts directory and named according to the YEAR-MONTH-DAY-title.MARKUP format as per the usual.   ","categories": [],
+        "tags": ["awk"],
+        "url": "/posts/awk",
+        "teaser": null
+      }]
